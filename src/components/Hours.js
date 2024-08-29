b7106ec974066eb73625f534a80ac2e9a99e8a71
cabrera1054 <49962108+cabrera1054@users.noreply.github.com>
2024-08-29 19:41:46 +0000

    cambios reto 2

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,15 @@
-// Placeholder to host a component
+import React from 'react';
+
+const Hours = () => {
+  const now = new Date();
+  const hours = now.getHours();
+
+  return (
+    <div>
+      <h2>Horas del día</h2>
+      <p>La hora actual es: {hours}</p>
+    </div>
+  );
+};
+
+export default Hours;
